036d5e6f67e5a49514e5de55165eb7295f440e5f
Muhammad Arslan <imarslan7061@gmail.com>
2025-12-19 00:58:22 +0500

    Update

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,28 @@
+import { useQuery } from "@tanstack/react-query";
+
 const App = () => {
-  return <div className="bg-black text-white p-2 font-medium text-2xl">TanStack Query App</div>;
+
+  const {data, isFetching, refetch, error} = useQuery({
+    queryKey: ["todos"],
+    queryFn: getTodos
+  });
+
+  if(error) {
+    return <div>Error occurred: {(error as Error).message}</div>
+  }
+
+  return <>
+  <div>
+    <div className="bg-black text-white p-2 font-medium text-2xl">TanStack Query App</div>
+    <div>{isFetching ? "Loading..." : JSON.stringify(data?.slice(0, 10))}</div>
+    <button onClick={() => refetch()}>Refetch</button>
+  </div>
+  </>;
 };
 
+const getTodos = async () => {
+  const response = await fetch("https://jsonplaceholder.typicode.com/todos");
+  return response.json();
+}
 export default App;
+ 
