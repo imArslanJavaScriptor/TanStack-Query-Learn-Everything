b2961b26467e53ee354f439542c0c8bf6ef403d1
Muhammad Arslan <imarslan7061@gmail.com>
2026-05-12 07:22:01 +0500

    feat: implement data fetching with TanStack Query
    Optimized data fetching by integrating TanStack Query to handle loading and error states efficiently.

--- src/components/Pages/FetchRQ.jsx
+++ src/components/Pages/FetchRQ.jsx
@@ -1,7 +1,41 @@
-const FetchRQ = () => {
-  return (
-    <div>FetchRQ</div>
-  )
-}
+import { fetchPosts } from "../../API/api-old";
+import { useQuery } from "@tanstack/react-query";
 
-export default FetchRQ
+const FetchRQ = () => {
+  
+  const getPostsData = async () => {
+    try {
+      const res = await fetchPosts();
+      return res.status === 200 ? res.data : [];
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const { data, isLoading, isError } = useQuery({
+    queryKey: ["posts"],
+    queryFn: getPostsData,
+  });
+
+  // Conditional rendering based on loading, error, and posts data
+  if (isLoading) return <p>Loading...</p>;
+  if (isError) return <p>Something went wrong!</p>;
+
+  return (
+    <div>
+      <ul className="section-accordion">
+        {data?.map((curElem) => {
+          const { id, title, body } = curElem;
+          return (
+            <li key={id}>
+              <p>{title}</p>
+              <p>{body}</p>
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
+};
+
+export default FetchRQ;
